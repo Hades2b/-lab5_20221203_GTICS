37d2b0c04c73cb28606342aab0f779f8b7b67313
Hades2b <a20221203@pucp.edu.pe>
2025-09-25 21:08:41 -0500

    commit

--- src/main/java/com/example/lab5_20221203/entity/Ranking.java
+++ src/main/java/com/example/lab5_20221203/entity/Ranking.java
@@ -3,9 +3,14 @@ package com.example.lab5_20221203.entity;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "ranking")
 public class Ranking {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer UserId;
 
+    @Column(name = "total_regalos")
     private Integer total;
 
     @ManyToOne(fetch = FetchType.EAGER)
